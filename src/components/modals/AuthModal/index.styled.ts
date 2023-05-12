95cf7c886d73c656b8c142a198774691d1ec1c5b
JaeilJo <whwodlf97@gmail.com>
2023-05-13 07:21:28 +0900

    feat : Create AuthModal

--- src/components/modals/AuthModal/index.styled.ts
+++ src/components/modals/AuthModal/index.styled.ts
@@ -1,7 +1,24 @@
 import { styled } from 'styled-components';
 
 const S = {
-    AuthModal: styled.div``,
+    ModalBackground: styled.div`
+        position: absolute;
+        backdrop-filter: blur(10px);
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    `,
+    AuthModal: styled.div`
+        background-color: white;
+        border: 1px solid ${({ theme }) => theme.LIGHT.COLOR.GRAY};
+        width: 400px;
+        height: 500px;
+        border-radius: 1em;
+    `,
 };
 
 export default S;
--- src/components/modals/AuthModal/index.tsx
+++ src/components/modals/AuthModal/index.tsx
@@ -2,7 +2,11 @@ import { ButtonProps } from './index.types';
 import S from './index.styled';
 
 const AuthModal = () => {
-    return <S.AuthModal></S.AuthModal>;
+    return (
+        <S.ModalBackground>
+            <S.AuthModal>Hello</S.AuthModal>
+        </S.ModalBackground>
+    );
 };
 
 export default AuthModal;
