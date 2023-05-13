d1072be228810647fd6d47ab55b7aeb542c698e9
JaeilJo <whwodlf97@gmail.com>
2023-05-14 02:42:58 +0900

    feat : add SSR in active page

--- src/pages/active.tsx
+++ src/pages/active.tsx
@@ -5,6 +5,9 @@ import { useAppDispatch, useAppSelector } from '../redux/hooks';
 import Main from '../components/organisms/Main';
 import TableMockDatas from '../components/organisms/Table/TableMockDatas';
 import AuthModal from '../components/modals/AuthModal';
+import { wrapper } from '../redux/store';
+import { authApi } from '../redux/services/authApi';
+import { setUser } from '../redux/features/userSlice';
 
 const Active: NextPage = () => {
     const dispatch = useAppDispatch();
@@ -20,5 +23,32 @@ const Active: NextPage = () => {
         </>
     );
 };
+
+export const getServerSideProps = wrapper.getServerSideProps((store) => async (context) => {
+    const cookie = context.req.cookies;
+
+    //refresh와  디코딩된 token얻어오기
+    const tokens = await store.dispatch(authApi.endpoints.refresh.initiate(cookie.rt));
+
+    //token없는 것에 대한 처리
+    if (!tokens || !tokens.data) return { props: {} };
+
+    //데이터 처리를 위한 준비
+    const refreshToken = tokens.data?.refreshToken;
+    const userData = {
+        data: {
+            userName: tokens.data?.userData.userName,
+            exp: tokens.data?.userData.exp,
+            token: tokens.data?.userData.token,
+        },
+    };
+
+    store.dispatch(setUser(userData));
+    context.res.setHeader('Set-Cookie', `rt=${refreshToken}; Path=/; HttpOnly`);
+
+    return {
+        props: {},
+    };
+});
 
 export default Active;
